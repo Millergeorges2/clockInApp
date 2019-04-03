3c31da70baac7f339774b09c89e90e3f2b069d4e
Miller <Miller@MacBook-Pro.local>
2019-04-03 13:16:41 -0700

    added create user page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,87 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+
 import './App.css';
+
+var Route = require('react-router-dom').Route;
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      workIdent: '0'
+      workIdent: '',
+      createID: '',
+      createName: ''
     }
-    
-     this.getWorkerID = this.getWorkerID.bind(this);
-     this.showWorkerID = this.showWorkerID.bind(this);
+
+    this.getWorkerID = this.getWorkerID.bind(this);
+    this.showWorkerID = this.showWorkerID.bind(this);
+    this.getCreatedID = this.getCreatedID.bind(this);
+    this.getCreatedName = this.getCreatedName.bind(this);
   }
 
-  getWorkerID(event)  {
-    console.log('change detected ' + event.target.value)
-    this.setState = ({ workIdent: event.target.value });    
+  getWorkerID(event) {
+    // console.log('change detected ' + event.target.value);
+    // console.log(`this.state is ${this.state.workIdent}`)
+    this.setState({ workIdent: event.target.value });
   }
 
   showWorkerID() {
-   console.log(`worker ID is ${this.state.workIdent}`)
+    //  console.log(`worker ID is ${this.state.workIdent}`)
   }
+
+  getCreatedName(event) {
+    this.setState({createName: event.target.value});
+  }
+
+  getCreatedID(event) {
+    this.setState({createID: event.target.value})
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <h1>Primo Pizza and Pasta</h1>
-        <h3> Please enter your ID Code</h3>
-        <div className="input-group mb-3">
-          <input onChange={this.getWorkerID}  type="number" className="form-control" aria-describedby="basic-addon2" />
-          <div className="input-group-append">
-            <button onClick={this.showWorkerID}  id="submitID" className="btn btn-outline-secondary" type="button">Button</button>
-          </div>
-        </div>
+      <Router>
+        <div className="App">
 
-      </div>
+          <Route path='/' exact render={
+            () => {
+              return (
+                <div>
+                  <h1>Primo Pizza and Pasta</h1>
+                  <h3> Please enter your ID Code</h3>
+                  <div className="input-group mb-3">
+                    <input onChange={this.getWorkerID} type="number" className="form-control" aria-describedby="basic-addon2" />
+                    <div className="input-group-append">
+                      <button onClick={this.showWorkerID} id="submitID" className="btn btn-outline-secondary" type="button"> <Link to='/createworker'> create User</Link></button>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          } />
+
+          <Route path='/createworker' exact render={
+            () => {
+              return (
+                <div>
+
+                  <h2> Create Worker Account</h2>
+                  <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="Name" />
+                  </div>
+                  <div class="form-group">
+                    <label for="createID">Create ID:</label>
+                    <input type="number" maxLength='6' class="form-control" id="createID" />
+                  </div>
+                </div>
+              )
+            }
+          } />
+        </div>
+      </Router>
     );
   }
 }
