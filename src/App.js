968977dc192a5c746db9e0979a05d6ce992253c6
Miller <Miller@MacBook-Pro.local>
2019-04-02 20:52:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+      ID: 0
+    })
+    
+     this.getWorkerID = this.getWorkerID.bind(this);
+     this.showWorkerID = this.showWorkerID.bind(this);
+  }
+
+  getWorkerID(event) {
+    this.setState = ({ ID: event.target.value });    
+  }
+
+  showWorkerID() {
+   console.log(`worker ID ${this.state.ID}`)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Primo Pizza and Pasta</h1>
+        <h3> Please enter your ID Code</h3>
+        <div className="input-group mb-3">
+          <input onChange={this.getWorkerID}  type="number" className="form-control" aria-describedby="basic-addon2" />
+          <div className="input-group-append">
+            <button onClick={this.showWorkerID}  id="submitID" className="btn btn-outline-secondary" type="button">Button</button>
+          </div>
+        </div>
+
       </div>
     );
   }
 }
+
 
 export default App;
